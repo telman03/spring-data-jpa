d06f6ef35fedeb6b549646b9b073e88389190989
telman03 <telmangadimov1@gmail.com>
2022-12-01 11:19:21 +0400

    Initial commit

---
+++ src/main/java/com/dailycodebuffer/tutorial/Entity/Student.java
@@ -0,0 +1,25 @@
+package com.dailycodebuffer.tutorial.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Student {
+    @Id
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
+}
